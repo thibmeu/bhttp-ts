ad1af0cbee22ec0049347a9bbbd8cfefb9669f8e
Thibault Meunier <thibault@cloudflare.com>
2026-03-09 10:28:58 +0100

    export streaming API from mod.ts

--- mod.ts
+++ mod.ts
@@ -1,3 +1,28 @@
 export { BHttpDecoder } from "./src/decoder.ts";
 export { BHttpEncoder } from "./src/encoder.ts";
 export * from "./src/errors.ts";
+
+// Streaming API
+export {
+	BHttpRequestStreamEncoder,
+	BHttpResponseStreamEncoder,
+} from "./src/stream-encoder.ts";
+export {
+	BHttpStreamDecoder,
+	type BHttpEvent,
+	type BHttpRequestPreambleEvent,
+	type BHttpResponsePreambleEvent,
+	type BHttpInformationalEvent,
+	type BHttpContentEvent,
+	type BHttpTrailersEvent,
+	type BHttpEndEvent,
+} from "./src/stream-decoder.ts";
+
+// VLI utilities (for advanced use)
+export {
+	encodeVli,
+	decodeVli,
+	vliEncodedLength,
+	vliExpectedLength,
+	type VliDecodeResult,
+} from "./src/vli.ts";
